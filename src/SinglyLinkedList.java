e93e1a1586499a05a8187e91a3110c7f487788f9
Dell <sandeep.dixit_cs17@gla.ac.in>
2019-02-27 14:27:03 +0530

    init commit

---
+++ src/SinglyLinkedList.java
@@ -0,0 +1,12 @@
+public class SinglyLinkedList {
+
+    private static class Node{
+        private int data;
+        private Node next;
+
+        public Node(int data, Node next){
+            this.data = data;
+            this.next = next;
+        }
+    }
+}
